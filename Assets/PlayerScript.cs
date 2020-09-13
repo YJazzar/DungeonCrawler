cdd6c6f229258bd4dfdb949df02f2ca2f9014f98
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-09-13 15:49:12 -0500

    Added walls

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -7,8 +7,7 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
 
-    public Vector3 speed = new Vector3(50, 50, 50);
-
+    public Vector3 speed = new Vector3(30, 30, 30);
 
     // Start is called before the first frame update
     void Start()
