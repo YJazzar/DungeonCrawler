dd790ec26bf5a7cb34cdeb505db3009fc2eec5a2
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-10-04 16:33:22 -0500

    Adding functionality for player switching objects

--- Assets/MainScript.cs
+++ Assets/MainScript.cs
@@ -2,17 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class MainScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject model1;
+    public GameObject model2;
+
+
+    private GameObject currentModel;
+    // private bool temp = false;
+
     void Start()
     {
-        Debug.Log("From main script!");
+        Debug.Log("main script running");
+
+        currentModel = Instantiate(model2, transform.position, transform.rotation) as GameObject;
+        currentModel.transform.parent = transform;
+
+
+        // model1.SetActive(false);
+        // model2.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.I))
+        {
+            Debug.Log("Changing model");
+            ChangeModel();
+        }
+    }
+
+    public void ChangeModel()
+    {
+        if (currentModel == model1)
+        {
+            GameObject thisModel = Instantiate(model2, transform.position, transform.rotation) as GameObject;
+            Destroy(currentModel);
+            thisModel.transform.parent = transform;
+            currentModel = thisModel;
+        }
+        else
+        {
+            GameObject thisModel = Instantiate(model1, transform.position, transform.rotation) as GameObject;
+            Destroy(currentModel);
+            thisModel.transform.parent = transform;
+            currentModel = thisModel;
+        }
     }
 }
