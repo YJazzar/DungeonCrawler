9c46670238f3785cca6baec3912441362a91cfbf
Shariff Habboush <shariff.habboush@yahoo.com>
2020-10-08 14:00:34 -0500

    Jump working correctly

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -11,6 +11,9 @@ public class PlayerScript : MonoBehaviour
     public Vector3 maxspeed = new Vector3(3, 3, 3);
     public double jumpTime = 1.5;
     public bool jump = false;
+    public float jumpHeight = 8;
+
+    public GameObject g;
 
     // Start is called before the first frame update
     void Start()
@@ -23,36 +26,47 @@ public class PlayerScript : MonoBehaviour
     {
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Jump");
-        // float inputZ = Input.GetAxis("Vertical");
 
 
-        Vector3 movement = new Vector3(speed.x * inputX, 0, 0);
+        Vector3 movement = new Vector3(speed.x * inputX, 0, speed.z * inputZ);
 
         movement *= Time.deltaTime;
         if (movement.x > maxspeed.x) {
             movement.x = 5;
         }
 
+        // position  -9.5 < x < 6
+        //             0 < y < 9.7
 
-        if (inputY > 0) { jump = true; }
+        // if (-9.5 < transform.Position.x < 6) {
 
-        if (transform.position.y < 1) {
-            jumpTime = 1.5;
-        }
-        else {
-            jumpTime = 0;
-        }
-        if (jump) {
-            movement.y = speed.y * inputY * Time.deltaTime;
-            jumpTime -= Time.deltaTime;
-            if (jumpTime < 0) { jump = false; }
+        // }
+        // Debug.Log(transform.position.x);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            g = GameObject.Find("Player");
+            Rigidbody r = GameObject.Find("Player").GetComponent<Rigidbody>();
+            r.velocity = new Vector3 (r.velocity.x, jumpHeight, r.velocity.z);
+            // .getComponent<Rigidbody>();
+            // r.velocity = new Vector3(r.velocity.x, 8, r.velocity.z);
         }
 
-        if (movement.z > maxspeed.z) {
-            movement.z = 5;
-        }
-        // Debug.Log(movement.x);
-        // Debug.Log(movement.z);
+        // if (inputY > 0) { jump = true; }
+
+        // if (transform.position.y < 1) {
+        //     jumpTime = 1.5;
+        // }
+        // else {
+        //     jumpTime = 0;
+        // }
+        // if (jump) {
+        //     movement.y = speed.y * inputY * Time.deltaTime;
+        //     jumpTime -= Time.deltaTime;
+        //     if (jumpTime < 0) { jump = false; }
+        // }
+
+        Debug.Log(movement.x);
+        Debug.Log(movement.z);
         transform.Translate(movement, Space.World);
     }
 
