0dec5b6cff76687b7a9583c751774338e1364f93
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-10-08 16:35:56 -0500

    Cleaned up code

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -17,23 +17,19 @@ public class PlayerScript : MonoBehaviour
 
 
     // Keymappings used for different actions: 
-    KeyCode changeModel = KeyCode.Space;
-    KeyCode rotateRight = KeyCode.E;
-    KeyCode rotateLeft = KeyCode.Q;
+    KeyCode changeModelButton = KeyCode.Space;
+    KeyCode rotateRightButton = KeyCode.E;
+    KeyCode rotateLeftButton = KeyCode.Q;
 
 
-    float tiltAngle = 0.0f;
-    float smooth = 5.0f;
-    bool rotatingLeft;
+    private float tiltAngle = 0.0f;
+    private float smooth = 5.0f;
+    private bool rotatingLeft;
     bool rotatingRight;
 
     public Vector3 speed = new Vector3(10, 10, 0);
     public Vector3 maxspeed = new Vector3(30, 30, 0);
-    public double jumpTime = 1.5;
-    public bool jump = false;
-    public float jumpHeight = 1;
 
-    public GameObject g;
 
     // Start is called before the first frame update
     void Start()
@@ -44,109 +40,72 @@ public class PlayerScript : MonoBehaviour
 
         loadPlayerModels();
 
-        ChangeModel();
+        changeModel();
     }
 
     public void Update()
     {
-        // Check if we need to change the model of the player
-        if (Input.GetKeyDown(changeModel))
+    
+        handleRotation();
+    }
+
+    public void FixedUpdate()
+    {
+        handleMovement();
+    }
+
+    // Check if we need to change the model of the player
+    // If the player did issue a command to change the model, then call the function ChangeModel()
+    public void handleModelChange()
+    {
+        
+        if (Input.GetKeyDown(changeModelButton))
         {
             currModelNumber = (++currModelNumber) % models.Count;
-            ChangeModel();
+            changeModel();
         }
-
-        handleRotation();
 
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void handleMovement()
     {
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
 
-
-
         Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
 
         movement *= Time.deltaTime;
-        if (movement.x > maxspeed.x) {
+        if (movement.x > maxspeed.x)
+        {
             movement.x = 5;
         }
-        if (movement.y > maxspeed.y) {
+        if (movement.y > maxspeed.y)
+        {
             movement.y = 5;
         }
 
-
-        // position  -9.5 < x < 6
-        //             0 < y < 9.7
-
-        // if (-9.5 < transform.Position.x < 6) {
-
-        // }
-        // Debug.Log(transform.position.x);
-
-        // if (Input.GetKey(KeyCode.Space)) {
-        //     g = GameObject.Find("Player");
-        //     Rigidbody r = GameObject.Find("Player").GetComponent<Rigidbody>();
-        //     if (r.velocity.y > maxspeed.y) { r.velocity = new Vector3 (r.velocity.x, r.velocity.y, r.velocity.z); }
-        //     else { r.velocity = new Vector3 (r.velocity.x, r.velocity.y + jumpHeight*inputY, r.velocity.z); }
-            // .getComponent<Rigidbody>();
-            // r.velocity = new Vector3(r.velocity.x, 8, r.velocity.z);
-        // }
-
-        // if (inputY > 0) { jump = true; }
-
-        // if (transform.position.y < 1) {
-        //     jumpTime = 1.5;
-        // }
-        // else {
-        //     jumpTime = 0;
-        // }
-        // if (jump) {
-        //     movement.y = speed.y * inputY * Time.deltaTime;
-        //     jumpTime -= Time.deltaTime;
-        //     if (jumpTime < 0) { jump = false; }
-        // }
-
-        // Debug.Log(movement.x);
-        // Debug.Log(movement.z);
         transform.Translate(movement, Space.World);
     }
 
-    void OnCollisionEnter(Collision collision) 
-     {
-         if(collision.gameObject.name == "Wall (Left)" || collision.gameObject.name == "Wall (Right)" || collision.gameObject.name == "Wall (Top)")  // or if(gameObject.CompareTag("YourWallTag"))
-         {
-            Debug.Log("Collision");
-            transform.Translate(Vector3.zero, Space.World);
-         }
-     }
 
-     // Rotates the player model as needed
-    public void handleRotation() {
+    // Rotates the player model as needed
+    public void handleRotation()
+    {
         // Left rotation
-        if (Input.GetKeyDown(rotateLeft))
+        if (Input.GetKeyDown(rotateLeftButton))
         {
             rotatingLeft = true;
-            rotatingRight = false; 
-        }
-
-        if (Input.GetKeyUp(rotateLeft))
-        {
-            rotatingLeft = false;
             rotatingRight = false;
         }
 
         // Right rotation
-        if (Input.GetKeyDown(rotateRight))
+        if (Input.GetKeyDown(rotateRightButton))
         {
             rotatingLeft = false;
             rotatingRight = true;
         }
 
-        if (Input.GetKeyUp(rotateRight))
+        if (Input.GetKeyUp(rotateLeftButton) || Input.GetKeyUp(rotateRightButton))
         {
             rotatingLeft = false;
             rotatingRight = false;
@@ -163,7 +122,7 @@ public class PlayerScript : MonoBehaviour
             transform.rotation = target;
 
             Debug.Log("Rotating NOW = " + tiltAngle);
-            tiltAngle+= 1;
+            tiltAngle += 1;
         }
 
 
@@ -178,11 +137,11 @@ public class PlayerScript : MonoBehaviour
             transform.rotation = target;
 
             Debug.Log("Rotating NOW = " + tiltAngle);
-            tiltAngle-= 1;
+            tiltAngle -= 1;
         }
     }
 
-    public void ChangeModel()
+    public void changeModel()
     {
         Debug.Log("Changing model to model number = " + currModelNumber);
         GameObject thisModel = Instantiate(models[currModelNumber], transform.position, transform.rotation) as GameObject;
@@ -192,7 +151,8 @@ public class PlayerScript : MonoBehaviour
     }
 
 
-    public void loadPlayerModels() {
+    public void loadPlayerModels()
+    {
         string sAssetFolderPath = "Assets/PlayerModels";
         string[] aux = sAssetFolderPath.Split(new char[] { '/' });
         string onlyFolderPath = aux[0] + "/" + aux[1] + "/";
@@ -215,5 +175,13 @@ public class PlayerScript : MonoBehaviour
 
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Wall (Left)" || collision.gameObject.name == "Wall (Right)" || collision.gameObject.name == "Wall (Top)")  // or if(gameObject.CompareTag("YourWallTag"))
+        {
+            Debug.Log("Collision");
+            transform.Translate(Vector3.zero, Space.World);
+        }
+    }
 
 }
